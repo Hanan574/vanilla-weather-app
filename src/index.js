3654ad13a634a6b634ebcfc84cf452d2a7bb433a
Hanan574 <hannimuhammad574@gmail.com>
2023-03-17 14:30:17 +0000

    integrated Axios and weather API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function weather(response) {
+  console.log(response);
+  document.querySelector("#temperature").innerHTML = `${Math.round(
+    response.data.main.temp
+  )}°C`;
+  document.querySelector("h1").innerHTML = `${response.data.name}`;
+  document.querySelector(
+    ".skyType"
+  ).innerHTML = `${response.data.weather[0].description}`;
+}
 
+let apiKey = "2e4de2b11b8a6f23faaa2d921b3e9a8f";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dubai&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(weather);
